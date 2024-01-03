a9c2e85d4e2f19419f480292caf225ad4cb2e6c1
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-03 14:41:25 +0100

    0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  return new Promise(() => {
+    // Your API logic here
+    // Call resolve() if the API call is successful
+    // Call reject() if there is an error
+  });
+}
