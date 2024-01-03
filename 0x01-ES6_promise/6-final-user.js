ec547e0853ed2f080d43899b44d6811b3e00a4c3
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-03 15:47:04 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => {
-      console.log(`${values[0].firstName} ${values[0].lastName} ${values[1].body}`);
-    })
-    .catch(() => console.log('Signup system offline'));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((values) =>()
+    values.map((result) => ({
+      status: result.status,
+      value:
+        result.status === 'fulfilled' ? result.value : String(result.reason),
+    })))
+  ));
 }
