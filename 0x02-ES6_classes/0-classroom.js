ad47be7878f644fe1a5cf33604b6a5a2ec203728
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-04 13:07:31 +0100

    0-classroom.js

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+export default class Classroom {
+  /**
+   * Represents a classroom.
+   * @constructor
+   * @param {number} maxStudentsSize - The maximum size of the classroom.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
