4d81d3363f6254e6dc1195b39806e1f7941acbf6
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-04 13:22:57 +0100

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -15,4 +15,39 @@ export default class HolbertonCourse {
       this._students = students;
     } else throw TypeError('Students must be an array of strings');
   }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else throw TypeError('Name must be a string');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else throw TypeError('Length must be a number');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (Array.isArray(students)) {
+      students.forEach((student) => {
+        if (typeof student !== 'string') {
+          throw TypeError('Students must be an array of strings');
+        }
+      });
+      this._students = students;
+    } else throw TypeError('Students must be an array of strings');
+  }
 }
