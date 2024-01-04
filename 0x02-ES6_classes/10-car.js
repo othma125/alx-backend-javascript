2b6ad96a37540a7c993c8b1eae8d7599fd32a104
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-04 18:35:03 +0100

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -3,30 +3,37 @@ export default class Car {
     if (typeof brand !== 'string') {
       throw TypeError('brand must be a string');
     }
+    // eslint-disable-next-line no-underscore-dangle
     this._brand = brand;
     if (typeof motor !== 'string') {
       throw TypeError('motor must be a string');
     }
+    // eslint-disable-next-line no-underscore-dangle
     this._motor = motor;
     if (typeof color !== 'string') {
       throw TypeError('color must be a string');
     }
+    // eslint-disable-next-line no-underscore-dangle
     this._color = color;
   }
 
   get brand() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._brand;
   }
 
   get motor() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._motor;
   }
 
   get color() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._color;
   }
 
   cloneCar() {
+    // eslint-disable-next-line no-underscore-dangle
     return new Car(this._brand, this._motor, this._color);
   }
 }
