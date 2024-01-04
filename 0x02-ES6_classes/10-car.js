cdddcb54844de2292c7368185d443f2d160671fa
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-04 19:36:01 +0100

    10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -8,16 +8,12 @@ export default class Car {
     this._color = color;
   }
 
-  // eslint-disable-next-line class-methods-use-this
   cloneCar() {
+    if (this.constructor.name === 'EVCar') {
+      // eslint-disable-next-line no-underscore-dangle
+      return new Car(this._brand, this._motor, this._color);
+    }
     // eslint-disable-next-line no-underscore-dangle
-    const newCar = new Car(this._brand, this._motor, this._color);
-    // eslint-disable-next-line no-underscore-dangle
-    newCar._brand = undefined;
-    // eslint-disable-next-line no-underscore-dangle
-    newCar._motor = undefined;
-    // eslint-disable-next-line no-underscore-dangle
-    newCar._color = undefined;
-    return newCar;
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
