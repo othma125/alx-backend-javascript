00ee8c0d03bdeffdaacfa10a296cd6133a00c7f5
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-04 18:38:47 +0100

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,21 +2,34 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
+    if (typeof range !== 'number') {
+      throw TypeError('range must be a number');
+    }
+    if (typeof brand !== 'string') {
+      throw TypeError('brand must be a string');
+    }
+    if (typeof motor !== 'string') {
+      throw TypeError('motor must be a string');
+    }
     super(brand, motor, color);
+    if (typeof color !== 'string') {
+      throw TypeError('color must be a string');
+    }
     this.range = range;
   }
 
   get range() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._range;
   }
 
   set range(value) {
+    // eslint-disable-next-line no-underscore-dangle
     this._range = value;
   }
 
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    const { brand, motor, color } = this;
+    return new Car(brand, motor, color);
   }
 }
