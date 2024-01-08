6c56c4651fd433b04a4d61d678d3b502fd626542
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-08 17:35:26 +0100

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
-  if (position >= length) {
+  if (position >= length || position < 0) {
     throw Error('Position outside range');
   }
   view[position] = value;
