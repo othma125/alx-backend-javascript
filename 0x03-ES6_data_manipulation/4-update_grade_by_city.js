9f20d71f987a1315607af58bcfffae7ba6924a92
Othmane EL YAAKOUBI <othma125@hotmail.com>
2024-01-08 16:27:29 +0100

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(
+  ListStudents,
+  city,
+  newGrades,
+) {
+  return ListStudents.filter((student) => student.location === city).map(
+    (student) => {
+      const grade = newGrades.filter(
+        (newGrade) => newGrade.studentId === student.id,
+      );
+      if (grade.length === 0) return { ...student, grade: 'N/A' };
+      return { ...student, grade: grade[0].grade };
+    },
+  );
+}
